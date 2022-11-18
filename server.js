fb3c325dc8a5f42740203ce6bdae675c78b15bf0
trygve <trygvepaulsen@me.com>
2022-11-18 20:29:42 +0100

    books create/index/new

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const connectDB = require('./db/connect')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -24,6 +25,7 @@ app.use(express.json())
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 
 // Server
